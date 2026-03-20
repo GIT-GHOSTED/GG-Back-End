afec276ef0b06863319b5f21d78415ee0b1401ac
Michael <michaelflorentino4444@gmail.com>
2026-03-20 12:49:47 -0400

    fixed local host connect

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: /localhost/ }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
